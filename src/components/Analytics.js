231954ce81a64a0b12c3109364094742d25c3b95
Nitin Sharma <nitinzeroit@gmail.com>
2025-11-12 11:06:37 +0530

    update-dashboard

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -70,7 +70,7 @@ const AnalyticsPage = () => {
                 }}
               />
               <InputGroup.Text>
-                <FontAwesomeIcon icon={faSearch} />
+                
               </InputGroup.Text>
             </InputGroup>
 
